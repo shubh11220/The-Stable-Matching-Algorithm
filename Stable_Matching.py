bfca366bd69042cd5ce1870a4906ae16afdf28bd
Shubhankar Singh <shubhankarsingh2001@gmail.com>
2020-07-21 12:18:57 +0530

    functional.py

--- Stable_Matching.py
+++ Stable_Matching.py
@@ -1,13 +1,10 @@
 
 
-
-
-
-# Part 1: Initialization
+# Initialization
 Men = ['Mike', 'Harvey', 'Louis', 'Logan']
 Women = ['Rachel', 'Donna', 'Katrina', 'Sheila']
 
-# Part 3: Preferences
+# Preferences
 Men_Pref = {  # indicates the preferences of the men
     'Mike':   ['Rachel', 'Katrina', 'Donna', 'Sheila'],
     'Harvey': ['Donna', 'Katrina', 'Rachel', 'Sheila'],
@@ -21,38 +18,44 @@ Women_Pref = {  # indicates the preferences of the women
     'Katrina': ['Mike', 'Harvey', 'Louis', 'Logan'],
     'Sheila':  ['Louis', 'Logan', 'Harvey', 'Mike']
 }
+def main():
+    Men_Free = list(Men)
+    Women_Free = list(Women)
 
-Men_Free = list(Men)
-Women_Free = list(Women)
+    # Part 3: Proposal
+    Matches = {
+        'Mike':  '',
+        'Harvey': '',
+        'Louis': '',
+        'Logan': ''
+        }
+    key_list = list(Matches.keys())
 
-# Part 3: Proposal
-Matches = {
-    'Mike':  '',
-    'Harvey': '',
-    'Louis': '',
-    'Logan': ''
-    }
-key_list = list(Matches.keys())
-# the algorithm
-while len(Men_Free) > 0:
-    for man in key_list:
-        for woman in Men_Pref[man]:
-            if woman not in list(Matches.values()):
-                Matches[man] = woman
-                Men_Free.remove(man)
-                print('{} is no longer a free man and is tentatively engaged to {} !'.format(man, woman))
-                break
-            elif woman in list(Matches.values()):
-                current_suitor = list(Matches.keys())[list(Matches.values()).index(woman)]
-                w_list = Women_Pref.get(woman)
-                if w_list.index(man) < w_list.index(current_suitor):
+    # the algorithm
+
+    while len(Men_Free) > 0:
+        for man in key_list:
+            for woman in Men_Pref[man]:
+                if woman not in list(Matches.values()):
                     Matches[man] = woman
                     Men_Free.remove(man)
-                    Matches[current_suitor] = ''
-                    Men_Free.append(current_suitor)
-                    print('{} was earlier engaged to {} but now is engaged to {}! '.format(woman, current_suitor, man))
+                    print('{} is no longer a free man and is tentatively engaged to {} !'.format(man, woman))
+                    break
+                elif woman in list(Matches.values()):
+                    current_suitor = list(Matches.keys())[list(Matches.values()).index(woman)]
+                    w_list = Women_Pref.get(woman)
+                    if w_list.index(man) < w_list.index(current_suitor):
+                        Matches[man] = woman
+                        Men_Free.remove(man)
+                        Matches[current_suitor] = ''
+                        Men_Free.append(current_suitor)
+                        print('{} was earlier engaged to {} but now is engaged to {}! '.format(woman, current_suitor, man))
 
-print('\n \n \n ')
-print('Stable Matching Finished ! Happy engagement !')
-for man in Matches.keys():
-    print('{} is engaged to {} !'.format(man, Matches[man]))
+    print('\n ')
+    print('Stable Matching Finished ! Happy engagement !')
+    for man in Matches.keys():
+        print('{} is engaged to {} !'.format(man, Matches[man]))
+
+
+if __name__ == "__main__":
+    main()
